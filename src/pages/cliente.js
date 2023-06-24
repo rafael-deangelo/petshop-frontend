a2719f9acadb506811b9b22d272d1f3bfe5d69b5
rafael-deangelo <rafaelangelo15@hotmail.com>
2023-06-24 12:52:36 -0300

    feat: remove some unused components

--- src/pages/cliente.js
+++ src/pages/cliente.js
@@ -1,14 +1,15 @@
-import React from 'react';
-import Title from './../components/Title/index';
-import SignUp from '../components/SignUp/index';
+import React from "react";
+import Title from "./../components/Title/index";
+import SignUp from "../components/SignUp/index";
 
 export default function Cliente() {
-    return (
-        <div>
-            <Title
-                title={"Criar uma conta"}
-                text={"Complete os campos com as suas informações"} />
-            <SignUp />
-        </div>
-    )
+  return (
+    <div>
+      <Title
+        title={"Criar uma conta"}
+        text={"Complete os campos com as suas informações"}
+      />
+      <SignUp />
+    </div>
+  );
 }
--- src/pages/pedido.js
+++ src/pages/pedido.js
@@ -1,14 +1,12 @@
-import React from 'react'
+import React from "react";
 import Title from "../components/Title";
-import Checkout from '../components/Checkout';
+import Checkout from "../components/Checkout";
 
 export default function Pedido() {
-    return (
-        <div>
-            <Title 
-            title={"Checkout"}
-            text={"Finalize seu pedido"} />
-            <Checkout />
-        </div>
-    )
+  return (
+    <div>
+      <Title title={"Checkout"} text={"Finalize seu pedido"} />
+      <Checkout />
+    </div>
+  );
 }
